26986fd4e2879087e9088269dfa7dc66fa54b8d5
unknown <webhostingportfolio124@gmail.com>
2026-02-22 14:40:30 +0530

    made frontend API URL dynamic for Docker

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const BASE_URL = "https://linkedin-clone-backend-o37d.onrender.com/"
+// export const BASE_URL = "https://linkedin-clone-backend-o37d.onrender.com/"
+
+export const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "https://linkedin-clone-backend-o37d.onrender.com/"
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
