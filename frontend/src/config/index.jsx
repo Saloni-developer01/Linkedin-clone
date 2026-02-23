62cad762a7ce4434a3b962a56eb18f4b3ffd3a67
unknown <webhostingportfolio124@gmail.com>
2026-02-23 20:01:54 +0530

    Fix: Next.js version downgrade, CORS setup, and Docker volumes fixed

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -2,8 +2,9 @@ import axios from "axios";
 
 // export const BASE_URL = "https://linkedin-clone-backend-o37d.onrender.com/"
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "https://linkedin-clone-backend-o37d.onrender.com/"
+export const BASE_URL = process.env.NEXT_LOCAL_BACKEND_URL || "https://linkedin-clone-backend-o37d.onrender.com/"
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
+    withCredentials: true
 });
